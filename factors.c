afd7b912a51dfad954c95d510a6218062f1a5b52
stebonicacid <stebonicacid@gmail.com>
2022-12-16 11:03:06 +0000

    RSA Factoring Challenge

--- factors.c
+++ factors.c
@@ -1,58 +1,145 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
-/**
- * read_textfile- read and sent to stdout a number of charss.
- *
- *@filename: name of file
- *@letters: number of letters
- * Return: Number of printed chars.
- */
-int main(int ac, char **av)
-{
-	int i, j, flag, fd, h;
-	int leido = 0;
-	long int number, number2, k;
-	char *buf, *buf2;
-	int letters = 100;
+#include "rsa.h"
+#include "pollard-rho.c"
 
-	leido = leido;
-	ac = ac;
-	number = 0;
-	fd = open(av[1], O_RDONLY);
-	buf = malloc(1000 * sizeof(char));
-	buf2 = malloc(120 * sizeof(char));
-	leido = read(fd, buf, letters);
- 	i = 0;
-	while (buf[i] != 0)
+/**
+ * init - Initializes the text buffer with null char
+ *
+ * @buffer: Array that will hold numbers
+ *
+ * Return: None (void function)
+ */
+void init(char *buffer)
+{
+	int i = 0;
+
+	for (i = 0; i < S_BUFFER; i++)
+		buffer[i] = '\0';
+}
+
+/**
+ * file2buffer - Reads argv[1] to buffer
+ *
+ * @filename: Pointer to file location
+ *
+ * @buffer: Array that holds the file data
+ *
+ * Return: None (void function)
+ */
+void file2buffer(char *filename, char *buffer)
+{
+	int opens = 0;
+
+	opens = open(filename, O_RDONLY);
+	if (opens < 0)
+		exit(EXIT_FAILURE);
+
+	if (read(opens, buffer, S_BUFFER) < 0)
 	{
-		j = 0;
-		for (h = 0; h <= 100; h++)
-			buf2[h] = 0;
-		while (buf[i] != 10)
-			 {
-				 buf2[j] = buf[i];
-				 i++;
-				 j++;
-			 }
-		number = atol(buf2);
-		flag = 0;
-		for (k = 2; k < number /2 && flag == 0; k++)
-		{
-			if (number % k == 0)
-				flag = 1;
-		}
-		number2 = number / (k - 1);
-		printf("%li=%li*%li\n", number, number2, k - 1);
-		i++;
+		close(opens);
+		exit(EXIT_FAILURE);
 	}
-	free(buf);
-	free(buf2);
-	close(fd);
-	return(0);
+	close(opens);
+}
+
+/**
+ * _atoi - ASCII -> int conversion
+ *
+ * @src: Input char array
+ *
+ * @dest: Converted data array
+ *
+ * Return: Number of elements copied
+ */
+long long unsigned int _atoi(char *src, long long unsigned int *dest)
+{
+	int i = 0;
+
+	for (i = 0; src[i] && src[i] != '\n'; i++)
+		dest[i] = src[i] - '0';
+
+	return (i);
+}
+
+/**
+ * _putint - General agnostic number printing function
+ *
+ * @dest: Array of newly converted data
+ *
+ * @n: Size of array (N is also the principle number in PRho)
+ *
+ * Return: None (void function)
+ */
+void _putint(long long unsigned int *dest, long long unsigned int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+		putchar(dest[i] + '0');
+}
+
+/**
+ * compute - Converts int array to int
+ *
+ * @src: Array of integers forming number
+ *
+ * @n: Number of digits in number
+ *
+ * Return: Integer repr. of array of ints
+ */
+long long unsigned int compute(long long unsigned int *src, long long unsigned int n)
+{
+        int i;
+	long long unsigned int result = 0;
+
+        for (i = 0; i < n; i++)
+	{
+                result *= 10;
+		result += src[i];
+	}
+
+	//printf("result is: %llu\n", result);
+	return result;
+}
+
+/**
+ * main - Entry point
+ *
+ * @argc: Size of argv array
+ *
+ * @argv: Array of CLI inputs
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	char buffer[S_BUFFER]; /* original buffer */
+	char *token; /* tokenized buffer */
+
+	long long unsigned int number[VALUE]; /* integer repr. of number */
+	long long unsigned int cv = 1;
+
+	long long unsigned int n; /* final value to compute */
+	long long unsigned int big = 1;
+
+	if (argc != 2)
+		return (EXIT_FAILURE);
+
+	init(buffer);
+	file2buffer(argv[1], buffer);
+	token = strtok(buffer, " \n"); /* separates each number */
+
+	while (token != NULL)
+	{
+		cv = _atoi(token, number); /* converted values */
+
+		n = compute(number, cv);
+		big = n / PollardRho(n);
+		printf("%llu=%llu*%llu\n", n, big, PollardRho(n));
+
+		//_putint(number, cv);
+		//putchar('\n');
+		token = strtok(NULL, " \n"); /* breaks up each number */
+	}
+
+	return (EXIT_SUCCESS);
 }
